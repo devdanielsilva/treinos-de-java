357dbf4c714bb7b95c0ddccbf60c967b15043407
Daniel Silva <devdanielsilva@gmail.com>
2024-03-26 13:57:35 -0300

    1°Programa em java
    O início da Jornada!

---
+++ 1°MeuPrimeiroPrograma/src/main/java/com/mycompany/meuprimeiroprograma/MeuPrimeiroPrograma.java
@@ -0,0 +1,8 @@
+package com.mycompany.meuprimeiroprograma;
+
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
